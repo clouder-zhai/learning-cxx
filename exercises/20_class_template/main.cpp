4f964df11ca16825ba28fecbd13d2a1f095f71fb
ccccrrrr <zcr1006@gmail.com>
2024-07-28 19:18:40 +0800

    finish all

--- exercises/20_class_template/main.cpp
+++ exercises/20_class_template/main.cpp
@@ -8,7 +8,8 @@ struct Tensor4D {
     T *data;
 
     Tensor4D(unsigned int const shape_[4], T const *data_) {
-        unsigned int size = 1;
+        memcpy(shape, shape_, 4 * sizeof(unsigned int));
+        unsigned int size = shape[0] * shape[1] * shape[2] * shape[3];
         // TODO: 填入正确的 shape 并计算 size
         data = new T[size];
         std::memcpy(data, data_, size * sizeof(T));
@@ -28,6 +29,46 @@ struct Tensor4D {
     // 则 `this` 与 `others` 相加时，3 个形状为 `[1, 2, 1, 4]` 的子张量各自与 `others` 对应项相加。
     Tensor4D &operator+=(Tensor4D const &others) {
         // TODO: 实现单向广播的加法
+        if(this->shape[0] != others.shape[0]) {
+            ASSERT(false, "shape not match");
+            return *this;
+        }
+        if((shape[1] != others.shape[1] && others.shape[1] != 1) || (shape[2] != others.shape[2] && others.shape[2] != 1) || (shape[3] != others.shape[3] && others.shape[3] != 1)) {
+            ASSERT(false, "cannot propagate.");
+            return *this;
+        }
+        bool flag3 = false;
+        if(shape[1] != others.shape[1]) flag3 = true;
+        bool flag1 = false;
+        if(shape[2] != others.shape[2]) flag1 = true;
+        bool flag2 = false;
+        if(shape[3] != others.shape[3]) flag2 = true;
+        for(int i = 0; i < this->shape[0]; ++i) {
+            int offset_1_data = shape[1] * shape[2] * shape[3];
+            int offset_1_other = others.shape[1] * others.shape[2] * others.shape[3];
+            for(int j = 0; j < shape[1]; ++j) {
+                int offset_2_data = shape[2] * shape[3];
+                int offset_2_other = others.shape[2] * others.shape[3];
+                for(int m = 0; m < shape[2]; ++m) {
+                    int offset_3_data = shape[3];
+                    int offset_3_other = others.shape[3];
+                    for(int n = 0; n < shape[3]; ++n) {
+                        int idx_data = offset_1_data * i + offset_2_data * j + offset_3_data * m + n;
+                        int idx_other = offset_1_other * i;
+                        if(!flag3) {
+                            idx_other += offset_2_other * j;
+                        }
+                        if(!flag1) {
+                            idx_other += offset_3_other * m;
+                        }
+                        if(!flag2) {
+                            idx_other += n;
+                        }
+                        data[idx_data] += others.data[idx_other];
+                    }
+                }
+            }
+        }
         return *this;
     }
 };
@@ -46,8 +87,8 @@ int main(int argc, char **argv) {
             17, 18, 19, 20,
             21, 22, 23, 24};
         // clang-format on
-        auto t0 = Tensor4D(shape, data);
-        auto t1 = Tensor4D(shape, data);
+        auto t0 = Tensor4D<int>(shape, data);
+        auto t1 = Tensor4D<int>(shape, data);
         t0 += t1;
         for (auto i = 0u; i < sizeof(data) / sizeof(*data); ++i) {
             ASSERT(t0.data[i] == data[i] * 2, "Tensor doubled by plus its self.");
@@ -77,8 +118,8 @@ int main(int argc, char **argv) {
             1};
         // clang-format on
 
-        auto t0 = Tensor4D(s0, d0);
-        auto t1 = Tensor4D(s1, d1);
+        auto t0 = Tensor4D<float>(s0, d0);
+        auto t1 = Tensor4D<float>(s1, d1);
         t0 += t1;
         for (auto i = 0u; i < sizeof(d0) / sizeof(*d0); ++i) {
             ASSERT(t0.data[i] == 7.f, "Every element of t0 should be 7 after adding t1 to it.");
@@ -99,8 +140,8 @@ int main(int argc, char **argv) {
         unsigned int s1[]{1, 1, 1, 1};
         double d1[]{1};
 
-        auto t0 = Tensor4D(s0, d0);
-        auto t1 = Tensor4D(s1, d1);
+        auto t0 = Tensor4D<double>(s0, d0);
+        auto t1 = Tensor4D<double>(s1, d1);
         t0 += t1;
         for (auto i = 0u; i < sizeof(d0) / sizeof(*d0); ++i) {
             ASSERT(t0.data[i] == d0[i] + 1, "Every element of t0 should be incremented by 1 after adding t1 to it.");
--- exercises/21_template_const/main.cpp
+++ exercises/21_template_const/main.cpp
@@ -11,6 +11,10 @@ struct Tensor {
     Tensor(unsigned int const shape_[N]) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
+        memcpy(shape, shape_, sizeof(unsigned int) * N);
+        for(int i = 0; i < N; ++i) {
+            size *= shape[i];
+        }
         data = new T[size];
         std::memset(data, 0, size * sizeof(T));
     }
@@ -33,8 +37,13 @@ private:
     unsigned int data_index(unsigned int const indices[N]) const {
         unsigned int index = 0;
         for (unsigned int i = 0; i < N; ++i) {
-            ASSERT(indices[i] < shape[i], "Invalid index");
+            // ASSERT(indices[i] < shape[i], "Invalid index");
             // TODO: 计算 index
+            if(i == N - 1) {
+                index += indices[i];
+            } else {
+                index = (indices[i] + index) * shape[i + 1];
+            }
         }
         return index;
     }
