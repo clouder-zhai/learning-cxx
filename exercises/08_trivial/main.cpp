b3d5e734d80f1410a18710ece9226d1ff45448bf
ccccrrrr <zcr1006@gmail.com>
2024-07-28 10:42:37 +0800

    add some solution

--- exercises/08_trivial/main.cpp
+++ exercises/08_trivial/main.cpp
@@ -20,6 +20,8 @@ int main(int argc, char **argv) {
     // NOTICE: C/C++ 中，读取未初始化的变量（包括结构体变量）是未定义行为
     // READ: 初始化的各种写法 <https://zh.cppreference.com/w/cpp/language/initialization>
     FibonacciCache fib;
+    fib.cache[0] = 0;
+    fib.cache[1] = 1;
     ASSERT(fibonacci(fib, 10) == 55, "fibonacci(10) should be 55");
     std::cout << "fibonacci(10) = " << fibonacci(fib, 10) << std::endl;
     return 0;
